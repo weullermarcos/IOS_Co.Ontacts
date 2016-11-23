47a5143fdc2502a34474a6198561bd9949855439
Weuller Marcos <weullermarcos@MacBook-Pro-de-Weuller.local>
2016-11-23 19:20:43 -0200

    Aplicando autolayout a imagem da lista

--- CoOntacts/CoOntacts/ContactTableViewCell.m
+++ CoOntacts/CoOntacts/ContactTableViewCell.m
@@ -14,7 +14,8 @@
     [super awakeFromNib];
     // Initialization code
     
-    self.contactImage.layer.cornerRadius = self.contactImage.frame.size.width / 2.0;
+    self.contactImage.layer.cornerRadius = self.contactImage.frame.size.height * 0.01;
+//    self.contactImage.layer.cornerRadius = 10;
     self.contactImage.clipsToBounds = YES;
 }
 
