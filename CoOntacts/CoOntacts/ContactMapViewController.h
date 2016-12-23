ad620656af9164f8c4caf089efc5c45e6bbf920d
Weuller Marcos <weullermarcos@MacBook-Pro-de-Weuller.local>
2016-12-23 16:19:44 -0200

    Adicionando Mapa

---
+++ CoOntacts/CoOntacts/ContactMapViewController.h
@@ -0,0 +1,18 @@
+//
+//  ContactMapViewController.h
+//  CoOntacts
+//
+//  Created by Weuller Marcos on 23/12/16.
+//  Copyright © 2016 Weuller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import MapKit;
+@import CoreLocation;
+
+@interface ContactMapViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet MKMapView *contactMap;
+
+
+@end
---
+++ CoOntacts/CoOntacts/ContactMapViewController.m
@@ -0,0 +1,43 @@
+//
+//  ContactMapViewController.m
+//  CoOntacts
+//
+//  Created by Weuller Marcos on 23/12/16.
+//  Copyright © 2016 Weuller. All rights reserved.
+//
+
+#import "ContactMapViewController.h"
+@import MapKit;
+@import CoreLocation;
+
+@interface ContactMapViewController ()
+
+@end
+
+@implementation ContactMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+    [self setTitle:@"Mapa"];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
